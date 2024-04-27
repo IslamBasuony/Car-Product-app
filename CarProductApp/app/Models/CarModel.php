97890b4e005b53d2eb851d113dc5685729ac73de
اسم المستخدم الخاص بك <eslam23basuony15>
2024-04-27 06:36:36 +0300

    create model from car

---
+++ CarProductApp/app/Models/CarModel.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CarModel extends Model
+{
+    use HasFactory;
+    protected $table = 'cars';
+    protected $fillable = [
+        'name',
+        'price',
+        'manufacturing_year',
+        'color',
+        'category',
+        'condition1',
+        'body',
+        'image',
+    ];
+}
